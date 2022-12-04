4580d6cf4680493e76c0eac8d65f20aae5bdb56e
dagbouj hatem <dagboujhatem@gmail.com>
2022-12-04 23:30:01 +0100

    fix top menu

--- src/modules/emergency/components/top-menu.tsx
+++ src/modules/emergency/components/top-menu.tsx
@@ -1,25 +1,19 @@
-import Image from 'next/image'
 import Link from 'next/link'
 
 function TopMenu() {
     return (
-        <div className="container mx-auto header">
-            <div className="flex">
+        <div className="header">
+            <div className="container mx-auto flex items-end h-575 pb-8">
                 <Link href="/emergency#donate-let-aseel-decide" scroll={true} passHref>
                     Donate &amp; Let ASEEL Decide
                 </Link>
-                <Link className='btn-white' href="/emergency#emergency-products" scroll={true} passHref>
+                <Link href="/emergency#emergency-products" scroll={true} passHref>
                     Donate a package
                 </Link>
-                <Link className='btn-white' href="/campaign/form" passHref>
+                <Link href="/campaign/form" passHref>
                     Start a campaingn
                 </Link>
             </div>
-            <picture className="play-store">
-                <source className='image' type="image/webp" src="https://aseelapp.com/media/er/er-main-banner.webp" />
-                <source className='image' type="image/jpg" src="https://aseelapp.com/media/er/er-main-banner.jpg" />
-                {/* <Image layout='fill' className="lazyload play-store loaded image" src="https://aseelapp.com/media/er/er-main-banner.jpg" data-src="/media/er/er-main-banner.jpg" alt="ASEEL App" loading="lazy" /> */}
-            </picture>
         </div>
     )
 }
--- src/modules/emergency/templates/emergency-template.tsx
+++ src/modules/emergency/templates/emergency-template.tsx
@@ -10,7 +10,7 @@ import TopMenu from "../components/top-menu"
 
 const EmergencyTemplate = () => {
     return (
-        <div className="flex-1 small:py-12 small:bg-gray-50">
+        <div className="flex-1 small:py-12 small:bg-gray-50 content">
             <TopMenu/>
             <Packages/>
             <Donate/>
