7b9f047dba27d32d1928d61a05529c2616f60db4
dagbouj hatem <dagboujhatem@gmail.com>
2022-12-05 20:55:23 +0100

    fix image responsive

--- src/modules/emergency/components/packages.tsx
+++ src/modules/emergency/components/packages.tsx
@@ -7,7 +7,9 @@ function Packages() {
             <div>
                 <div className="flex-container">
                     <div className="flex-item">
-                        <Image className="lazyload border-radius-5 loaded" width='321' height='321' src="https://aseelapp.com/media/catalog/product/c/h/child_relief.jpg" data-src="https://aseelapp.com/media/catalog/product/c/h/child_relief.jpg" alt="image item" />
+                        <div className="package-image">
+                            <Image layout='fill' objectFit='fill' src="https://aseelapp.com/media/catalog/product/c/h/child_relief.jpg" alt="image item" />
+                        </div>
                         <div className="package-block">
                             <p><strong>Emergency Package for Child Relief</strong></p>
                             <p><strong>USD 61</strong></p>
@@ -15,7 +17,9 @@ function Packages() {
                         </div>
                     </div>
                     <div className="flex-item">
-                        <Image className="lazyload border-radius-5 loaded" width='321' height='321' src="https://aseelapp.com/media/catalog/product/w/i/winter_packages_1.jpg" data-src="https://aseelapp.com/media/catalog/product/w/i/winter_packages_1.jpg" alt="image item" />
+                        <div className="package-image">
+                            <Image layout='fill' objectFit='fill' src="https://aseelapp.com/media/catalog/product/w/i/winter_packages_1.jpg"  alt="image item" />
+                        </div>
                         <div className="package-block">
                             <p><strong>Large Emergency Winter Package</strong></p>
                             <p><strong>USD 95</strong></p>
@@ -23,7 +27,9 @@ function Packages() {
                         </div>
                     </div>
                     <div className="flex-item">
-                        <Image className="lazyload border-radius-5 loaded" width="321" height="321" src="https://aseelapp.com/media/catalog/product/e/r/er_food.jpg" data-src="https://aseelapp.com/media/catalog/product/e/r/er_food.jpg" alt="image item" />
+                        <div className="package-image">
+                            <Image layout='fill' objectFit='fill' src="https://aseelapp.com/media/catalog/product/e/r/er_food.jpg" alt="image item" />
+                        </div>
                         <div className="package-block">
                             <p><strong>Emergency Food Package</strong></p>
                             <p><strong>USD 92</strong></p>
@@ -31,7 +37,9 @@ function Packages() {
                         </div>
                     </div>
                     <div className="flex-item">
-                        <Image className="lazyload border-radius-5 loaded" width="321" height="321" src=" https://aseelapp.com/media/catalog/product/b/a/baby_1.jpg" data-src=" https://aseelapp.com/media/catalog/product/b/a/baby_1.jpg" alt="image item" />
+                        <div className="package-image">
+                            <Image layout='fill' objectFit='fill' src="https://aseelapp.com/media/catalog/product/b/a/baby_1.jpg"  alt="image item" />
+                        </div>
                         <div className="package-block">
                             <p><strong>Emergency Baby Care Package</strong></p>
                             <p><strong>USD 46</strong></p>
@@ -39,7 +47,9 @@ function Packages() {
                         </div>
                     </div>
                     <div className="flex-item">
-                        <Image className="lazyload border-radius-5 loaded" width="321" height="321" src=" https://aseelapp.com/media/catalog/product/l/i/life_package.jpg" data-src=" https://aseelapp.com/media/catalog/product/l/i/life_package.jpg" alt="image item" />
+                        <div className="package-image">
+                            <Image layout='fill' objectFit='fill' src="https://aseelapp.com/media/catalog/product/l/i/life_package.jpg" alt="image item" />
+                        </div>
                         <div className="package-block">
                             <p><strong>Emergency Life Package</strong></p>
                             <p><strong>USD 211</strong></p>
@@ -47,7 +57,9 @@ function Packages() {
                         </div>
                     </div>
                     <div className="flex-item">
-                        <Image className="css lazyload border-radius-5 loaded" width="321" height="321" src=" https://aseelapp.com/media/catalog/product/f/i/first_aid.jpg" data-src=" https://aseelapp.com/media/catalog/product/f/i/first_aid.jpg" alt="image item" />
+                        <div className="package-image">
+                            <Image layout='fill' objectFit='fill' src="https://aseelapp.com/media/catalog/product/f/i/first_aid.jpg" alt="image item" />
+                        </div>
                         <div className="package-block">
                             <p><strong>Emergency First Aid Package</strong></p>
                             <p><strong>USD 46</strong></p>
